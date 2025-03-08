68720ea6c24066b656761dabea0baa4864e72158
Jeremy <jeremy.dryden@gmail.com>
2025-03-07 23:11:29 -0500

    added a datatable with players. Sorting and pagination isn't working

--- src/components/containers/table/columns.tsx
+++ src/components/containers/table/columns.tsx
@@ -1,49 +1,6 @@
 import { ColumnDef } from "@tanstack/react-table";
-
-const unfilteredPlayers = [
-  {
-    id: 8474668,
-    headshot: "https://assets.nhle.com/mugs/nhl/20112012/VAN/8474668.png",
-    firstName: {
-      default: "Dale",
-    },
-    lastName: {
-      default: "Weise",
-    },
-    sweaterNumber: 32,
-    positionCode: "R",
-    shootsCatches: "R",
-    heightInInches: 74,
-    weightInPounds: 196,
-    heightInCentimeters: 188,
-    weightInKilograms: 89,
-    birthDate: "1988-08-05",
-    birthCity: {
-      default: "Winnipeg",
-    },
-    birthCountry: "CAN",
-    birthStateProvince: {
-      default: "Manitoba",
-    },
-    hrID: null,
-    hdbID: null,
-    verified: false,
-  },
-].map((player) => {
-  return {
-    playerID: player.id,
-    firstName: player.firstName.default,
-    lastName: player.lastName.default,
-    birthDate: player.birthDate,
-    birthCity: player.birthCity.default,
-    birthCountry: player.birthCountry,
-    hrID: player.hrID,
-    hdbID: player.hdbID,
-    verified: player.verified,
-  };
-});
-
-console.log("P:", unfilteredPlayers);
+import { ArrowUpDown } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
 export type Player = {
   playerID: number;
@@ -57,11 +14,24 @@ export type Player = {
   verified: boolean;
 };
 
-export const players: Player[] = unfilteredPlayers;
+// export const players: Player[] = unfilteredPlayers;
 
 export const columns: ColumnDef<Player>[] = [
   { accessorKey: "playerID", header: "PlayerID" },
-  { accessorKey: "firstName", header: "First Name" },
+  {
+    accessorKey: "firstName",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          First Name
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+  },
   { accessorKey: "lastName", header: "Last Name" },
   { accessorKey: "birthDate", header: "BirthDate" },
   { accessorKey: "birthCity", header: "City" },
--- src/pages/table-test.tsx
+++
@@ -1,2 +0,0 @@
-import { Player, columns } from "@/components/containers/table/columns";
-import { DataTable } from "@/components/containers/table/data-table";
