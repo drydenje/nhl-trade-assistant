0555ed63df61ddfd49a9f0cd147b7feaeb27c1d0
Jeremy <jeremy.dryden@gmail.com>
2025-05-03 23:58:54 -0400

    adding more test cases, how do I mock a uuid?

--- src/stores/__tests__/playerStore.test.ts
+++ src/stores/__tests__/playerStore.test.ts
@@ -16,7 +16,11 @@ import {
   getTestStorage,
 } from "@nanostores/persistent";
 
-import { currentPlayer, playersToUpdate } from "../playerStore";
+import {
+  currentPlayer,
+  playersToUpdate,
+  addPlayerToUpdate,
+} from "../playerStore";
 
 // beforeAll runs once, beforeEach resets each time
 beforeAll(async (context) => {
@@ -50,41 +54,80 @@ describe("currentPlayer", () => {
   });
 });
 
-describe("playersToUpdate", () => {
-  // it("listens for changes", () => {
-  //   setTestStorageKey("settings:locale", "ru");
-  //   expect(currentPlayer.get()).toEqual({ locale: "ru" });
-  // });
-  // it("listens for changes", () => {
-  //   const players = [
-  //     {
-  //       uuid: "38ea29d3-572c-4182-98cd-31aff1bb8857",
-  //       playerId: 8482713,
-  //       name: "Matthew Samoskevich",
-  //       birthCity: "Newtown",
-  //       birthDate: "2002-11-15",
-  //       hdb: "223446",
-  //     },
-  //     {
-  //       uuid: "750bdeab-eb05-4fec-a28f-83e9e261b503",
-  //       playerId: 8484145,
-  //       name: "Zachary Benson",
-  //       birthCity: "Chilliwack",
-  //       birthDate: "2005-05-12",
-  //       hdb: "240680",
-  //     },
-  //     {
-  //       uuid: "b6d803ee-50e3-4eec-97af-08bc35418126",
-  //       playerId: 8482460,
-  //       name: "Matthew Rempe",
-  //       birthCity: "Calgary",
-  //       birthDate: "2002-06-29",
-  //       hdb: "216250",
-  //     },
-  //   ];
-  //   setTestStorageKey("playersToUpdate", JSON.stringify(players));
-  //   const p = JSON.parse(getTestStorage().playersToUpdate);
-  //   // console.log("P:", p);
-  //   expect(p).toEqual(players);
+describe("addPlayerToUpdate", () => {
+  it.only("takes a player object (hockeyDB) and adds it to the playersToUpdate store", () => {
+    const rempe = {
+      uuid: "26e81d12-692f-47f1-8542-043629280a88",
+      playerId: 8482460,
+      name: "Matt Rempe",
+      birthCity: "Calgary",
+      birthDate: "2002-06-29",
+      birthCountry: "CAN",
+      id: 216250,
+    };
+
+    const expected = [
+      {
+        uuid: "bdce13e4-6a85-4948-ade6-18a41a920e47",
+        playerId: 8482460,
+        name: "Matt Rempe",
+        birthCity: "Calgary",
+        birthDate: "2002-06-29",
+        hdbId: 216250,
+      },
+    ];
+    addPlayerToUpdate(rempe.playerId, rempe);
+    expect(playersToUpdate.get()).toBe(expected);
+  });
+
+  // it("takes a player object (hockey reference) and adds it to the playersToUpdate store", () => {
+  //   const rempe = {
+  //     uuid: "26e81d12-692f-47f1-8542-043629280a88",
+  //     playerId: 8482460,
+  //     name: "Matt Rempe",
+  //     birthCity: "Calgary",
+  //     birthDate: "2002-06-29",
+  //     birthCountry: "CAN",
+  //     hrId: "rempema01",
+  //   };
   // });
 });
+
+// describe("playersToUpdate", () => {
+// it("listens for changes", () => {
+//   setTestStorageKey("settings:locale", "ru");
+//   expect(currentPlayer.get()).toEqual({ locale: "ru" });
+// });
+// it("listens for changes", () => {
+//   const players = [
+//     {
+//       uuid: "38ea29d3-572c-4182-98cd-31aff1bb8857",
+//       playerId: 8482713,
+//       name: "Matthew Samoskevich",
+//       birthCity: "Newtown",
+//       birthDate: "2002-11-15",
+//       hdb: "223446",
+//     },
+//     {
+//       uuid: "750bdeab-eb05-4fec-a28f-83e9e261b503",
+//       playerId: 8484145,
+//       name: "Zachary Benson",
+//       birthCity: "Chilliwack",
+//       birthDate: "2005-05-12",
+//       hdb: "240680",
+//     },
+//     {
+//       uuid: "b6d803ee-50e3-4eec-97af-08bc35418126",
+//       playerId: 8482460,
+//       name: "Matthew Rempe",
+//       birthCity: "Calgary",
+//       birthDate: "2002-06-29",
+//       hdb: "216250",
+//     },
+//   ];
+//   setTestStorageKey("playersToUpdate", JSON.stringify(players));
+//   const p = JSON.parse(getTestStorage().playersToUpdate);
+//   // console.log("P:", p);
+//   expect(p).toEqual(players);
+// });
+// });
